975e5697967f7fa8e32ad2e6503e9dcf0db79b26
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-08-03 19:11:19 -0700

    otra modificacion

--- Api/settings.py
+++ Api/settings.py
@@ -84,13 +84,14 @@ WSGI_APPLICATION = 'Api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 """
 DATABASES = {
     'default': {
@@ -105,6 +106,7 @@ DATABASES = {
         }
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
